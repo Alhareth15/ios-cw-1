f79819ce172a03297d09e53e808e22453c7574fc
Alhareth15 <100311546+Alhareth15@users.noreply.github.com>
2022-03-02 19:43:23 +0300

    done

---
+++ myFirstapp/myFirstapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myFirstapp
+//
+//  Created by Alhareth Almulla on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("alhareth")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myFirstapp/myFirstapp/myFirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstappApp.swift
+//  myFirstapp
+//
+//  Created by Alhareth Almulla on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct myFirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
